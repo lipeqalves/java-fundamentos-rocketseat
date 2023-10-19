6c5cf61e726ce975dd6e0762ee70551c333ea28d
Filipe Alves <lipeqa@gmail.com>
2023-10-19 09:13:56 -0300

    feat: wrapper classes

---
+++ wrapper/wrapper.java
@@ -0,0 +1,31 @@
+package wrapper;
+
+public class wrapper {
+    public static void main(String[] args) {
+        int tipoInt = 1;
+        float tipoFloat = 1F;
+        boolean tipoBoolean = false;
+
+        /**
+         * Para usar os wrapper, é da mesma forma mais com a primeira letra maiscula
+         * com excessão do int e char
+         * pode definir valor nulo
+         * usa mais memoria na aplicação
+        */
+        /**
+         * float -> Float
+         * bollean -> Boolean
+         */
+
+        Float tipoFloat2 = 1F;
+        Boolean tipBoolean2 = false;
+
+        //int -> Integer
+
+        Integer tipoInteger = 1;
+
+        
+
+
+    }
+}
