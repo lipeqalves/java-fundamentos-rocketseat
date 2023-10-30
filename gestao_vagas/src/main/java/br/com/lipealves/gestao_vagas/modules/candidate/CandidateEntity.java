ba1b1ede6d085aa7a376386ec326057d82c93bde
Filipe Alves <lipeqa@gmail.com>
2023-10-30 09:49:47 -0300

    Java_ignite: Gestão de vagas - Inserindo validação

--- gestao_vagas/src/main/java/br/com/lipealves/gestao_vagas/modules/candidate/CandidateEntity.java
+++ gestao_vagas/src/main/java/br/com/lipealves/gestao_vagas/modules/candidate/CandidateEntity.java
@@ -2,6 +2,10 @@ package br.com.lipealves.gestao_vagas.modules.candidate;
 
 import java.util.UUID;
 
+import org.hibernate.validator.constraints.Length;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 
@@ -10,9 +14,16 @@ public class CandidateEntity {
 
     private UUID id;
     private String name;
+
+    @Pattern(regexp = "^(?!\\s*$).+", message = "O campo [usename] não deve conter espaçoes")
     private String username;
+
+    @Email(message ="O campo [email] de conter um e-mail válido")
     private String email;
+    
+    @Length(min = 10, max = 100)
     private String password;
+
     private String description;
     private String curriculum;
 
