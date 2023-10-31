ec49182e86e107fd5890fefaeb1d54587b5f85bb
Filipe Alves <lipeqa@gmail.com>
2023-10-31 09:12:42 -0300

    Java_ignite: Primeiros passo - Criando tabela de candidato

--- gestao_vagas/src/main/java/br/com/lipealves/gestao_vagas/modules/candidate/CandidateEntity.java
+++ gestao_vagas/src/main/java/br/com/lipealves/gestao_vagas/modules/candidate/CandidateEntity.java
@@ -1,9 +1,15 @@
 package br.com.lipealves.gestao_vagas.modules.candidate;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.validator.constraints.Length;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
@@ -11,9 +17,13 @@ import lombok.Data;
 
 
 @Data
+@Entity(name="candidate")
 public class CandidateEntity {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
+    
     private String name;
 
     @NotBlank
@@ -29,4 +39,6 @@ public class CandidateEntity {
     private String description;
     private String curriculum;
 
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 }
